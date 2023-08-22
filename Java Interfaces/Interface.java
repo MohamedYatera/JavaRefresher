b6cbdad827ef7a026be0c5e7089a9885d6218391
MohamedYatera <97811837+MohamedYatera@users.noreply.github.com>
2023-08-22 16:48:19 -0400

    Update Interface.java

--- Java Interfaces/Interface.java
+++ Java Interfaces/Interface.java
@@ -1,13 +1,42 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) {
-        List<Integer> integerList = new ArrayList<>();
-        integerList.add(5);
-        integerList.add(10);
+public class MyListImplementation<E> implements List<E> {
+    private ArrayList<E> internalList = new ArrayList<>();
 
-        System.out.println(integerList.size()); // Output: 2
-        System.out.println(integerList.get(0)); // Output: 5
+    @Override
+    public int size() {
+        return internalList.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return internalList.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return internalList.contains(o);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return internalList.add(e);
+    }
+
+    @Override
+    public E get(int index) {
+        return internalList.get(index);
+    }
+
+    // Implement the other List methods...
+
+    public static void main(String[] args) {
+        List<Integer> myList = new MyListImplementation<>();
+        myList.add(5);
+        myList.add(10);
+
+        System.out.println("Size: " + myList.size()); // Output: Size: 2
+        System.out.println("Element at index 1: " + myList.get(1)); // Output: Element at index 1: 10
     }
 }
