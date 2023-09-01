827c71bd0d4b96f81d15c1fe58fa74b8a19dee1e
MohamedYatera <97811837+MohamedYatera@users.noreply.github.com>
2023-09-01 14:27:22 -0400

    Create Autoboxing.java

---
+++ WrapperClass(Boxing)/Autoboxing.java
@@ -0,0 +1,5 @@
+// Another manual way of boxing bu its deprecated (old), rarely approriate
+Intger boxedInt = new Integer(15)
+
+// Java has autoboxing which is preferred, to manually boxing
+  Integer boxed Int = 15;
